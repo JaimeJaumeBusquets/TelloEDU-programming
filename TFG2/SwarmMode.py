c831154fa9ac2959679276bf7de8347103752c80
JaimeJaumeBusquets <98536492+JaimeJaumeBusquets@users.noreply.github.com>
2022-01-27 16:18:50 +0100

    Update SwarmMode.py

--- TFG2/SwarmMode.py
+++ TFG2/SwarmMode.py
@@ -5,8 +5,8 @@ me = tello.Tello()
 me.connect()
 print(me.get_battery())
 
-ssid = "MIWIFI_dd6r"
-password = "RJEY6pCJ"
+ssid = "    "
+password = "    "
 
 #me.set_wifi_credentials(ssid, password)
 me.connect_to_wifi(ssid, password)
